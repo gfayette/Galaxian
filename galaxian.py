7c5a3c63d242f554653a05ad9450c4ec91c425e5
G-Fayette <32397749+G-Fayette@users.noreply.github.com>
2020-03-22 18:24:56 -0400

    It's finished

--- galaxian.py
+++ galaxian.py
@@ -1,30 +1,44 @@
 #!/usr/bin/env python3
-
+"""
+George Fayette
+Justin Keller
+CIS 343
+Arcade Game in Python
+3-22-2020
+"""
 import pygame
 import random
 import sys
 
-
+# Class for handling the game overlay
 class Overlay(pygame.sprite.Sprite):
     def __init__(self):
+        pygame.init()
+        pygame.key.set_repeat(50)
+        pygame.mixer.music.load('assets/loop.wav')
+        pygame.mixer.music.play(-1)
         super(pygame.sprite.Sprite, self).__init__()
         self.image = pygame.Surface((800, 20))
         self.image.fill((0, 0, 0))
         self.rect = self.image.get_rect()
         self.font = pygame.font.Font('freesansbold.ttf', 18)
-        self.render('Score: 0        Lives: 5')
+        self.text = self.font.render('', True, (255, 255, 255))
 
+    # Change the text on the overlay
     def render(self, text):
-        self.text = self.font.render('Galaxian!', True, (255, 255, 255))
+        self.text = self.font.render(text, True, (255, 255, 255))
         self.image.blit(self.text, self.rect)
 
+    # Draw the overlay
     def draw(self, screen):
         screen.blit(self.text, (0, 0))
 
+    # Update score and lives
     def update(self, score, lives):
         self.render('Score: ' + str(score) + '        Lives: ' + str(lives))
 
 
+# Class for handling the player's ship
 class Ship(pygame.sprite.Sprite):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
@@ -33,10 +47,12 @@ class Ship(pygame.sprite.Sprite):
         self.rect.x = 375
         self.rect.y = 500
 
+    # Draw the ship
     def draw(self, screen):
         screen.blit(self.image, self.rect)
 
 
+# Class for handling enemy ships
 class EnemyShip(pygame.sprite.Sprite):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
@@ -45,6 +61,7 @@ class EnemyShip(pygame.sprite.Sprite):
         self.offset = 0
         self.velocity = 1
 
+    # Update the enemy ship's position and possibly fire a laser
     def update(self, game, blocks, enemy_lasers):
         if abs(self.offset) > 60:
             self.velocity = self.velocity * -1
@@ -57,38 +74,69 @@ class EnemyShip(pygame.sprite.Sprite):
             enemy_lasers.add(enemy_laser)
 
 
+# Class for handling the player's lasers
 class Laser(pygame.sprite.Sprite):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
         self.image = pygame.image.load(r'assets/laser.png')
         self.rect = self.image.get_rect()
 
-    def update(self, game, blocks, paddle):
+    # Update the player's laser and detect collisions
+    def update(self, game, enemy_ships, ship):
         if self.rect.y < 0:
             self.kill()
-        hitObject = pygame.sprite.spritecollideany(self, blocks)
-        if hitObject:
+        hit_object = pygame.sprite.spritecollideany(self, enemy_ships)
+        if hit_object:
             self.kill()
-            hitObject.kill()
+            hit_object.kill()
             game.score += 1
         self.rect.y -= 10
 
 
+# Class for handling enemy lasers
 class EnemyLaser(pygame.sprite.Sprite):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
         self.image = pygame.image.load(r'assets/enemy_laser.png')
         self.rect = self.image.get_rect()
 
-    def update(self, game, blocks, paddle):
+    # Update the enemy laser and detect collisions
+    def update(self, game, enemy_ships, ship):
         if self.rect.y > 600:
             self.kill()
-        if pygame.sprite.collide_rect(self, paddle):
+        if pygame.sprite.collide_rect(self, ship):
             self.kill()
             pygame.event.post(game.new_life_event)
-        self.rect.y += 10
+        self.rect.y += 5
 
 
+# The player shoots a laser
+def shoot(game, x, y):
+    laser = Laser()
+    laser.rect.x = game.ship.rect.x + x
+    laser.rect.y = game.ship.rect.y + y
+    game.lasers.add(laser)
+
+
+# Draw the screen
+def draw(game, dead):
+    game.overlay.draw(game.screen)
+    game.lasers.draw(game.screen)
+    game.enemy_lasers.draw(game.screen)
+    game.enemy_ships.draw(game.screen)
+    if not dead:
+        game.ship.draw(game.screen)
+    pygame.display.flip()
+
+
+# Display a message
+def message(game, text, dead):
+    game.overlay.render(text)
+    draw(game, dead)
+    pygame.time.delay(1000)
+
+
+# Class for handling the game events and game mechanics
 class Game:
     def __init__(self):
         self.clock = pygame.time.Clock()
@@ -101,16 +149,18 @@ class Game:
         self.overlay = Overlay()
         self.screen.fill((255, 255, 255))
         self.game_started = False
+        self.upgraded = False
         self.score = 0
-        self.lives = 500000
+        self.lives = 5
         self.enemy_offset = 0
         self.enemy_velocity = 5
-        for i in range(0, 4):
+        for i in range(0, 3):
             for j in range(0, 6):
                 block = EnemyShip()
                 block.rect.x = j * 120 + 50
-                block.rect.y = i * 80
+                block.rect.y = i * 80 + 40
                 self.enemy_ships.add(block)
+        self.overlay.render('Galaxian!')
 
     def run(self):
         while True:
@@ -120,8 +170,10 @@ class Game:
                     self.lives -= 1
                     self.game_started = False
                     if self.lives <= 0:
+                        message(self, 'You Lose!', True)
                         pygame.quit()
                         sys.exit(0)
+                    message(self, 'You Died!', True)
                 if event.type == pygame.QUIT:
                     pygame.quit()
                     sys.exit(0)
@@ -130,52 +182,35 @@ class Game:
                         self.game_started = True
                     else:
                         if event.key == pygame.K_SPACE:
-                            ball = Laser()
-                            ball.rect.x = self.ship.rect.x + 44
-                            ball.rect.y = self.ship.rect.y
-                            self.lasers.add(ball)
+                            shoot(self, 44, 0)
+                            if self.upgraded:
+                                shoot(self, 88, 25)
+                                shoot(self, 0, 25)
                         if event.key == pygame.K_LEFT:
                             self.ship.rect.x -= 12
                             if self.ship.rect.x <= 0:
                                 self.ship.rect.x = 0
                         if event.key == pygame.K_RIGHT:
                             self.ship.rect.x += 12
-                            if self.ship.rect.x >= 750:
-                                self.ship.rect.x = 750
+                            if self.ship.rect.x >= 700:
+                                self.ship.rect.x = 700
             if self.game_started:
+                if len(self.enemy_ships) < 10 and not self.upgraded:
+                    message(self, 'Weapons Upgrade!', False)
+                    self.upgraded = True
                 if len(self.enemy_ships) == 0:
+                    message(self, 'You Win!', False)
                     pygame.quit()
                     sys.exit(0)
                 self.enemy_ships.update(self, self.enemy_ships, self.enemy_lasers)
-                self.lasers.update(self, self.enemy_ships, self.ship)
                 self.enemy_lasers.update(self, self.enemy_ships, self.ship)
                 self.overlay.update(self.score, self.lives)
-                self.lasers.draw(self.screen)
-                self.enemy_lasers.draw(self.screen)
-                self.ship.draw(self.screen)
-                self.enemy_ships.draw(self.screen)
-                self.overlay.draw(self.screen)
-                pygame.display.flip()
-                self.clock.tick(60)
+                self.lasers.update(self, self.enemy_ships, self.ship)
+            draw(self, False)
+            self.clock.tick(60)
 
 
-class Intro(pygame.sprite.Sprite):
-    def __init__(self):
-        super(pygame.sprite.Sprite, self).__init__()
-        self.image = pygame.Surface((800, 20))
-        self.image.fill((0, 0, 0))
-        self.rect = self.image.get_rect()
-        self.font = pygame.font.Font('freesansbold.ttf', 18)
-
-    def draw(self, screen):
-        screen.blit(self.text, (0, 0))
-
-
+# Starting point for the program
 if __name__ == "__main__":
-    pygame.init()
-    pygame.key.set_repeat(50)
-    pygame.mixer.music.load('assets/loop.wav')
-    pygame.mixer.music.play(-1)
-    intro = Intro()
     game = Game()
     game.run()
